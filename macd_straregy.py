067a6b494aac969db53acf2c4cdca1160a133c4b
krezy50 <krezy50@naver.com>
2023-03-04 16:55:46 +0900

    macd_update

--- macd_straregy.py
+++ macd_straregy.py
@@ -69,7 +69,197 @@ class Mystrategy(bt.Strategy):
         dt = self.datas[0].datetime.date(0)
         st.write(f'[{dt.isoformat()}] {txt}')
 
+def macd_rsi(s, date):
 
+    st.subheader(f"분석 종목 : {s}")
+    f1 = pdr.get_data_yahoo(s, date)
+
+    f1.index = f1.index.date  # index 시간제거
+    df = pd.DataFrame(f1)
+
+    ema12 = df.Close.ewm(span=12).mean()  # 종가의 12일 지수 이동평균
+    ema26 = df.Close.ewm(span=26).mean()  # 종가의 26일 지수 이동평균
+    macd = ema12 - ema26  # macd 선
+    signal = macd.ewm(span=9).mean()  # 신호선 (macd의 9일 지수 이동평균)
+    macdhist = macd - signal
+
+    period = 14
+    delta = df.Close.diff()
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    gain = up.ewm(com=(period - 1), min_periods=period).mean()
+    loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
+    RS = gain / loss
+    rsi = 100 - (100 / (1 + RS))
+
+    df = df.assign(ema26=ema26, ema12=ema12, macd=macd, signal=signal, macdhist=macdhist, rsi=rsi).dropna()
+
+    df['number'] = df.index.map(mdates.date2num)  # 캔들차트에 사용할수 있게 날짜형 인덱스를 숫자형으로 변환
+    ohlc = df[['number', 'Open', 'High', 'Low', 'Close']]
+
+    plt.figure(figsize=(9, 9))
+
+    p1 = plt.subplot(3, 1, 1)
+    plt.title(f'MACD / MACD Osillator / RSI Trading {s}')
+    plt.grid(True)
+    candlestick_ohlc(p1, ohlc.values, width=.6, colorup='red',
+                     colordown='blue')  # ohlc의 숫자형 일자,시가,고가,저가,종가 값을 이용해서 캔들차트를 그린다.
+    p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.plot(df.number, df['ema26'], color='c', label='EMA26')
+    plt.plot(df.number, df['ema12'], color='g', label='EMA12', linestyle='--')
+    plt.legend(loc='best')
+
+    p2 = plt.subplot(3, 1, 2)
+
+    plt.grid(True)
+    p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.bar(df.number, df['macdhist'], color='m', label='MACD-Hist')
+    plt.plot(df.number, df['macd'], color='b', label='MACD')
+    plt.plot(df.number, df['signal'], 'g--', label='MACD-signal')
+
+    for i in range(1, len(df.Close)):
+        if df.macdhist.values[i - 1] < df.macdhist.values[i] and df.rsi.values[i] < 45:
+            plt.plot(df.number.values[i], 0, 'r^')
+        elif df.macdhist.values[i - 1] > df.macdhist.values[i] and df.rsi.values[i] > 60:
+            plt.plot(df.number.values[i], 0, 'bv')
+
+    plt.legend(loc='best')
+
+    p2 = plt.subplot(3, 1, 3)
+    plt.grid(True)
+    p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.plot(df.number, df['rsi'], color='b', label='%RSI')
+    plt.axhline(60, 0, 1, color='green', linestyle='--')
+    plt.axhline(45, 0, 1, color='green', linestyle='--')
+    plt.yticks([20, 30, 40, 50, 60, 70, 80])
+    plt.legend(loc='best')
+    figure = plt.show()
+    st.pyplot(figure)
+
+    st.subheader("BackTest 결과")
+    # Use a backend that doesn't display the plot to the user
+    # we want only to display inside the Streamlit page
+    matplotlib.use('Agg')
+
+    cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+    cerebro.addstrategy(Mystrategy)  # Add the trading strategy
+
+    feed = bt.feeds.PandasData(dataname=yf.download(s, date))
+
+    # st.write(data)
+    cerebro.adddata(feed)  # Add the data feed
+    cerebro.broker.setcash(10000000)
+    cerebro.broker.setcommission(commission=0.0014)
+
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=90)  # 매매 단위
+
+    st.write(f'Initial Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
+    cerebro.run()  # run it all
+
+    st.write(f'Final Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
+
+def macd_rsi_variation(s, date):
+
+    st.subheader(f"분석 종목 : {s}")
+    f1 = pdr.get_data_yahoo(s, date)
+
+    f1.index = f1.index.date  # index 시간제거
+    df = pd.DataFrame(f1)
+
+    ema12 = df.Close.ewm(span=12).mean()  # 종가의 12일 지수 이동평균
+    ema26 = df.Close.ewm(span=26).mean()  # 종가의 26일 지수 이동평균
+    macd = ema12 - ema26  # macd 선
+    signal = macd.ewm(span=9).mean()  # 신호선 (macd의 9일 지수 이동평균)
+    macdhist = macd - signal
+
+    period = 14
+    delta = df.Close.diff()
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    gain = up.ewm(com=(period - 1), min_periods=period).mean()
+    loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
+    RS = gain / loss
+    rsi = 100 - (100 / (1 + RS))
+
+    df = df.assign(ema26=ema26, ema12=ema12, macd=macd, signal=signal, macdhist=macdhist, rsi=rsi).dropna()
+
+    RSI_THRESHOLD = 0.20 #rsi 평균에 %15
+    rsi_ewm = df.rsi.ewm(span=52).mean() #rsi 지난 2개월 평균값
+    rsi_max_threshold = rsi_ewm + (rsi_ewm * RSI_THRESHOLD)
+    rsi_min_threshold = rsi_ewm - (rsi_ewm * RSI_THRESHOLD)
+
+    df = df.assign(rsi_ewm=rsi_ewm,rsi_max_threshold=rsi_max_threshold,rsi_min_threshold=rsi_min_threshold).dropna()
+
+    df['number'] = df.index.map(mdates.date2num)  # 캔들차트에 사용할수 있게 날짜형 인덱스를 숫자형으로 변환
+    ohlc = df[['number', 'Open', 'High', 'Low', 'Close']]
+
+    plt.figure(figsize=(9, 9))
+
+    p1 = plt.subplot(3, 1, 1)
+    plt.title(f'MACD / MACD Osillator / RSI Trading {s}')
+    plt.grid(True)
+    candlestick_ohlc(p1, ohlc.values, width=.6, colorup='red',
+                     colordown='blue')  # ohlc의 숫자형 일자,시가,고가,저가,종가 값을 이용해서 캔들차트를 그린다.
+    p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.plot(df.number, df['ema26'], color='c', label='EMA26')
+    plt.plot(df.number, df['ema12'], color='g', label='EMA12', linestyle='--')
+    plt.legend(loc='best')
+
+    p2 = plt.subplot(3, 1, 2)
+
+    plt.grid(True)
+    p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.bar(df.number, df['macdhist'], color='m', label='MACD-Hist')
+    plt.plot(df.number, df['macd'], color='b', label='MACD')
+    plt.plot(df.number, df['signal'], 'g--', label='MACD-signal')
+
+
+
+    for i in range(1, len(df.Close)):
+        if df.macdhist.values[i - 1] > df.macdhist.values[i] and df.rsi.values[i] > df.rsi_max_threshold.values[i-2]:
+            plt.plot(df.number.values[i], 0, 'bv')
+        elif df.macdhist.values[i - 1] < df.macdhist.values[i] and df.rsi.values[i] < df.rsi_min_threshold.values[i-2]:
+            plt.plot(df.number.values[i], 0, 'r^')
+
+    plt.legend(loc='best')
+
+    p2 = plt.subplot(3, 1, 3)
+    plt.grid(True)
+    p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    plt.plot(df.number, df['rsi'], color='b', label='%RSI')
+    plt.plot(df.number, df['rsi_ewm'], color='r', label='%RSI_ewm')
+    plt.plot(df.number, df['rsi_max_threshold'], 'g--', label='%RSI_max_threshold')
+    plt.plot(df.number, df['rsi_min_threshold'], 'g--', label='%RSI_min_threshold')
+    # plt.axhline(df.rsi_ewm.values[i]+RSI_THRESHOLD, 0, 1, color='green', linestyle='--')
+    # plt.axhline(df.rsi_ewm.values[i]-RSI_THRESHOLD, 0, 1, color='green', linestyle='--')
+    plt.yticks([20, 30, 40, 50, 60, 70, 80])
+    plt.legend(loc='best')
+    figure = plt.show()
+    st.pyplot(figure)
+
+    st.subheader("BackTest 결과")
+    # Use a backend that doesn't display the plot to the user
+    # we want only to display inside the Streamlit page
+    matplotlib.use('Agg')
+
+    cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+    cerebro.addstrategy(Mystrategy)  # Add the trading strategy
+
+    feed = bt.feeds.PandasData(dataname=yf.download(s, date))
+
+    # st.write(data)
+    cerebro.adddata(feed)  # Add the data feed
+    cerebro.broker.setcash(10000000)
+    cerebro.broker.setcommission(commission=0.0014)
+
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=90)  # 매매 단위
+
+    st.write(f'Initial Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
+    cerebro.run()  # run it all
+
+    st.write(f'Final Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
 
 
 def MACDStrategy():
@@ -81,215 +271,17 @@ def MACDStrategy():
 
     selected = st.checkbox("보유종목")
     date = st.date_input("시작날짜", datetime.date(2022, 10, 1))
+
     if selected:
-        list = ['AAPL', 'ABBV', 'GOOGL', 'INTC', 'NAIL', 'SOXL', 'SOXS']
+        list = ['AAPL', 'ABBV', 'GOOGL', 'INTC', 'NAIL', 'SOXL', 'SOXS','TSM']
 
         # 실제 코드
         for s in list:
-            st.subheader(f"분석 종목 : {s}")
-            f1 = pdr.get_data_yahoo(s, date)
 
-            f1.index = f1.index.date  # index 시간제거
-            df = pd.DataFrame(f1)
-
-
-            ema12 = df.Close.ewm(span=12).mean()  # 종가의 12일 지수 이동평균
-            ema26 = df.Close.ewm(span=26).mean()  # 종가의 26일 지수 이동평균
-            macd = ema12 - ema26  # macd 선
-            signal = macd.ewm(span=9).mean()  # 신호선 (macd의 9일 지수 이동평균)
-            macdhist = macd - signal
-
-            period=14
-            delta = df.Close.diff()
-            up, down = delta.copy(), delta.copy()
-            up[up<0]=0
-            down[down>0]=0
-            gain = up.ewm(com=(period-1), min_periods=period).mean()
-            loss = down.abs().ewm(com=(period-1), min_periods=period).mean()
-            RS = gain/loss
-            rsi= 100 - (100/(1+RS))
-
-            df = df.assign(ema26=ema26, ema12=ema12, macd=macd, signal=signal, macdhist=macdhist, rsi=rsi).dropna()
-
-            df['number'] = df.index.map(mdates.date2num)  # 캔들차트에 사용할수 있게 날짜형 인덱스를 숫자형으로 변환
-            ohlc = df[['number', 'Open', 'High', 'Low', 'Close']]
-
-            plt.figure(figsize=(9, 9))
-
-            p1 = plt.subplot(3, 1, 1)
-            plt.title(f'MACD / MACD Osillator / RSI Trading {s}')
-            plt.grid(True)
-            candlestick_ohlc(p1, ohlc.values, width=.6, colorup='red',
-                             colordown='blue')  # ohlc의 숫자형 일자,시가,고가,저가,종가 값을 이용해서 캔들차트를 그린다.
-            p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-            plt.plot(df.number, df['ema26'], color='c', label='EMA26')
-            plt.plot(df.number, df['ema12'], color='g', label='EMA12',linestyle ='--')
-            plt.legend(loc='best')
-
-
-            p2 = plt.subplot(3, 1, 2)
-
-            plt.grid(True)
-            p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-            plt.bar(df.number, df['macdhist'], color='m', label='MACD-Hist')
-            plt.plot(df.number, df['macd'], color='b', label='MACD')
-            plt.plot(df.number, df['signal'], 'g--', label='MACD-signal')\
-
-            for i in range(1, len(df.Close)):
-                if df.macdhist.values[i - 1] < df.macdhist.values[i] and df.rsi.values[i]<45 :
-                    plt.plot(df.number.values[i], 0, 'r^')
-                elif df.macdhist.values[i - 1] > df.macdhist.values[i] and df.rsi.values[i]>60 :
-                    plt.plot(df.number.values[i], 0, 'bv')
-
-            plt.legend(loc='best')
-
-            p2 = plt.subplot(3, 1, 3)
-            plt.grid(True)
-            p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-            plt.plot(df.number, df['rsi'], color='b', label='%RSI')
-            plt.axhline(60,0,1,color = 'green', linestyle ='--')
-            plt.axhline(45,0,1,color = 'green', linestyle ='--')
-            plt.yticks([20,30,40,50,60,70, 80])
-            plt.legend(loc='best')
-            figure = plt.show()
-            st.pyplot(figure)
-
-            st.subheader("BackTest 결과")
-            # Use a backend that doesn't display the plot to the user
-            # we want only to display inside the Streamlit page
-            matplotlib.use('Agg')
-
-            cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
-            cerebro.addstrategy(Mystrategy)  # Add the trading strategy
-
-            feed = bt.feeds.PandasData(dataname=yf.download(s, date))
-
-            # st.write(data)
-            cerebro.adddata(feed)  # Add the data feed
-            cerebro.broker.setcash(10000000)
-            cerebro.broker.setcommission(commission=0.0014)
-
-            cerebro.addsizer(bt.sizers.PercentSizer, percents=90)  # 매매 단위
-
-            st.write(f'Initial Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
-            cerebro.run()  # run it all
-
-            st.write(f'Final Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
-
-            # cerebro.plot()
-            #
-            # figure = cerebro.plot(style='candlestick')[0][0] #캔들차트로 설정
-            #
-            # # show the plot in Streamlit
-            # st.pyplot(figure)
+            macd_rsi_variation(s, date)
 
     else:
         stock = st.text_input("분석종목: ", value='SOXL')
 
-        f1 = pdr.get_data_yahoo(stock, date)
-
-        day_list = []
-        name_list = []
-
-        for i, day in enumerate(f1.index):
-            if day.dayofweek == 0:
-                day_list.append(i)
-                name_list.append(day.strftime('%Y-%m-%d'))
-
-        f1.index = f1.index.date  # index 시간제거
-        df = pd.DataFrame(f1)
-
-        ema12 = df.Close.ewm(span=12).mean()  # 종가의 12일 지수 이동평균
-        ema26 = df.Close.ewm(span=26).mean()  # 종가의 26일 지수 이동평균
-        macd = ema12 - ema26  # macd 선
-        signal = macd.ewm(span=9).mean()  # 신호선 (macd의 9일 지수 이동평균)
-        macdhist = macd - signal
-
-        period = 14
-        delta = df.Close.diff()
-        up, down = delta.copy(), delta.copy()
-        up[up < 0] = 0
-        down[down > 0] = 0
-        gain = up.ewm(com=(period - 1), min_periods=period).mean()
-        loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
-        RS = gain / loss
-        rsi = 100 - (100 / (1 + RS))
-
-        df = df.assign(ema26=ema26, ema12=ema12, macd=macd, signal=signal, macdhist=macdhist, rsi=rsi).dropna()
-        df['number'] = df.index.map(mdates.date2num)  # 캔들차트에 사용할수 있게 날짜형 인덱스를 숫자형으로 변환
-        ohlc = df[['number', 'Open', 'High', 'Low', 'Close']]
-
-        plt.figure(figsize=(9, 9))
-
-        p1 = plt.subplot(3, 1, 1)
-        plt.title(f'MACD / MACD Osillator / RSI Trading {stock}')
-        plt.grid(True)
-
-        #p1.xaxis.set_major_locator(ticker.FixedLocator(day_list))
-        #p1.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
-        candlestick_ohlc(p1, ohlc.values, width=.5, colorup='red',
-                         colordown='blue')  # ohlc의 숫자형 일자,시가,고가,저가,종가 값을 이용해서 캔들차트를 그린다.
-
-        p1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-        plt.plot(df.number, df['ema26'], color='c', label='EMA26')
-        plt.plot(df.number, df['ema12'], color='g', label='EMA12', linestyle='--')
-        plt.legend(loc='best')
-
-        p2 = plt.subplot(3, 1, 2)
-        plt.grid(True)
-        #p2.xaxis.set_major_locator(ticker.FixedLocator(day_list))
-        #p2.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
-        p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-        plt.bar(df.number, df['macdhist'], color='m', label='MACD-Hist')
-        plt.plot(df.number, df['macd'], color='b', label='MACD')
-        plt.plot(df.number, df['signal'], 'g--', label='MACD-signal') \
-
-        for i in range(1, len(df.Close)):
-            if df.macdhist.values[i - 1] < df.macdhist.values[i] and df.rsi.values[i] < 45:
-                plt.plot(df.number.values[i], 0, 'r^')
-            elif df.macdhist.values[i - 1] > df.macdhist.values[i] and df.rsi.values[i] > 60:
-                plt.plot(df.number.values[i], 0, 'bv')
-
-        plt.legend(loc='best')
-
-        p2 = plt.subplot(3, 1, 3)
-        plt.grid(True)
-        #p2.xaxis.set_major_locator(ticker.FixedLocator(day_list))
-        #p2.xaxis.set_major_formatter(ticker.FixedFormatter(name_list))
-        p2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-        plt.plot(df.number, df['rsi'], color='b', label='%RSI')
-        plt.axhline(60, 0, 1, color='green', linestyle='--')
-        plt.axhline(45, 0, 1, color='green', linestyle='--')
-        plt.yticks([20, 30, 40, 50, 60, 70, 80])
-        plt.legend(loc='best')
-        figure = plt.show()
-        st.pyplot(figure)
-
-        st.subheader("BackTest 결과")
-        # Use a backend that doesn't display the plot to the user
-        # we want only to display inside the Streamlit page
-        matplotlib.use('Agg')
-
-        cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
-        cerebro.addstrategy(Mystrategy)  # Add the trading strategy
-
-        feed = bt.feeds.PandasData(dataname=yf.download(stock, date))
-
-        # st.write(data)
-        cerebro.adddata(feed)  # Add the data feed
-        cerebro.broker.setcash(10000000)
-        cerebro.broker.setcommission(commission=0.0014)
-
-        cerebro.addsizer(bt.sizers.PercentSizer, percents=90)  # 매매 단위
-        #cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='areturn')
-        st.write(f'Initial Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
-        cerebro.run()  # run it all
-
-        st.write(f'Final Porfolio Value : {cerebro.broker.getvalue():,.0f} USD')
-
-        cerebro.plot()
-        #
-        figure = cerebro.plot(style='candlestick')[0][0] #캔들차트로 설정
-        #
-        # # show the plot in Streamlit
-        st.pyplot(figure)
+        # macd_rsi(stock,date)
+        macd_rsi_variation(stock,date)
